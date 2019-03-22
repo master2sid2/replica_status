d9164f00d8c9719310511b0dd40b7fdab3fc10ad
v.khutornyy <master2sid2@i.ua>
2019-03-22 15:26:19 +0200

    Add UI and basic function

--- main.py
+++ main.py
@@ -1,50 +1,72 @@
 #!/usr/bin/python3.6m
 
-import os
-import mysql.connector
-from os.path import join, dirname
+import design
+import sys, os, mysql.connector
+from PyQt5 import QtWidgets
 from dotenv import load_dotenv
+from os.path import join, dirname
 from sshtunnel import SSHTunnelForwarder
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
-ssh_host=os.getenv('SSH_HOST')
-ssh_port=os.getenv('SSH_PORT')
-username=os.getenv('SSH_LOGIN')
-password=os.getenv('SSH_PASS')
-mysql_host=os.getenv('MYSQL_HOST')
-mysql_port=os.getenv('MYSQL_PORT')
-mysql_username=os.getenv('MYSQL_USER')
-mysql_password=os.getenv('MYSQL_PASS')
-mysql_db=os.getenv('MYSQL_DB')
+class ReplicationStatus(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.label.setText("null")
+        self.label_2.setText("null")
+        self.label_3.setText("null")
+        self.label_4.setText("null")
 
-def getStatus():
-    print("Connect to mysql server", mysql_host)
-    mydb = mysql.connector.connect(host=mysql_host,
-                                   port=int(mysql_port),
-                                   user=mysql_username,
-                                   password=mysql_password)
-    cursor = mydb.cursor()
-    cursor.execute("show slave status")
-    result = cursor.fetchall()
-    result = str(result)
-    result = result.split(",")
-    print("Slave IO Running: ", result[10])
-    print("Slave SQL Running: ", result[11])
-    print("Error №: ", result[65])
-    print("Error: ", result[66])
-    print("Seconds Behind Master: ", result[79])
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
 
-server = SSHTunnelForwarder(
-    (ssh_host, int(ssh_port)),
-    ssh_username=username,
-    ssh_password=password,
-    remote_bind_address=('0.0.0.0', 3306),
-    local_bind_address=('0.0.0.0', 13306)
-)
+    ssh_host = os.getenv('SSH_HOST')
+    ssh_port = os.getenv('SSH_PORT')
+    username = os.getenv('SSH_LOGIN')
+    password = os.getenv('SSH_PASS')
+    mysql_host = os.getenv('MYSQL_HOST')
+    mysql_port = os.getenv('MYSQL_PORT')
+    mysql_username = os.getenv('MYSQL_USER')
+    mysql_password = os.getenv('MYSQL_PASS')
+    mysql_db = os.getenv('MYSQL_DB')
 
-server.start()
 
-print(server.local_bind_port)
-getStatus()
+
+    def createTunnel(self):
+        server = SSHTunnelForwarder(
+            (self.ssh_host, int(self.ssh_port)),
+            ssh_username=self.username,
+            ssh_password=self.password,
+            remote_bind_address=('0.0.0.0', 3306),
+            local_bind_address=('0.0.0.0', 13306)
+        )
+        server.start()
+
+    def getStatus(self):
+        print("Connect to mysql server", self.mysql_host)
+        mydb = mysql.connector.connect(host=self.mysql_host,
+                                       port=int(self.mysql_port),
+                                       user=self.mysql_username,
+                                       password=self.mysql_password)
+        cursor = mydb.cursor()
+        cursor.execute("show slave status")
+        result = cursor.fetchall()
+        result = str(result)
+        result = result.split(",")
+        self.label.setText(result[10])
+        self.label_2.setText(result[11])
+        self.label_3.setText(result[65])
+        self.label_4.setText(result[79])
+        cursor.close()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ReplicationStatus()
+    window.createTunnel()
+    window.getStatus()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+   main()
